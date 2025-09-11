3504914ff896357476690a24df5361e7a781b74b
Gokcejan <78448282+Gokcejan@users.noreply.github.com>
2025-09-11 13:43:32 +0200

    Add UserPrincipal to security context

--- server/src/main/java/cz/demo/librarymanagement/application/servicelayer/CustomUserDetailsService.java
+++ server/src/main/java/cz/demo/librarymanagement/application/servicelayer/CustomUserDetailsService.java
@@ -1,11 +1,15 @@
 package cz.demo.librarymanagement.application.servicelayer;
 
+import cz.demo.librarymanagement.application.domain.User;
+import cz.demo.librarymanagement.application.domain.UserPrincipal;
 import cz.demo.librarymanagement.application.domain.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class CustomUserDetailsService implements UserDetailsService {
@@ -15,7 +19,13 @@ public class CustomUserDetailsService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return userRepository.findOneByUsername(username)
-                .orElseThrow(() -> new UsernameNotFoundException("User not found: " + username));
+        User user = findUser(username);
+
+        return new UserPrincipal(user);
+    }
+
+    private User findUser(String username) {
+        Optional<User> versionOptional = userRepository.findOneByUsername(username);
+        return versionOptional.orElseThrow(() -> new UsernameNotFoundException("Username not found " + username));
     }
 }
